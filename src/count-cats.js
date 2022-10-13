e0063dc62aa1d046b3d771944eb70dd2bc3ebab5
olya <olyamolchanova322@gmail.com>
2022-10-13 13:33:56 +0300

    feat: two tasks are ready

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let val = matrix.flat();
+  let result = 0;
+  for(let i = 0; i < val.length; i++){
+    if(val[i] === "^^"){
+      result = result + 1
+    }
+  }
+  return result
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,57 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+/* function createDreamTeam(members) {
+  console.log(members);
+  let firstLetter = [];
+  for(let i = 0; i < members.length; i++) {
+     if(members[i][0] != " "){
+      firstLetter.push(members[i][0])
+    }
+    else if(members[i][0] === " "){
+      for(let d = 1; d < members[i].length; i++){
+        if(members[i][d] != " "){
+          firstLetter.push(members[i][d])
+          break
+        }
+      }
+    } 
+  }
+  return firstLetter.sort().join("").toUpperCase();
+} */
+
+function createDreamTeam(members) {
+  if(!members){
+    return false
+  }
+  let firstLetter = [];
+  for (let i = 0; i < members.length; i++) {
+    if(typeof members[i] != 'string'){
+      continue
+    }
+    else if (members[i][0] != " ") {
+      firstLetter.push(members[i][0].toUpperCase());
+    } else if (members[i][0] === " ") {
+      let w = members[i].trim();
+      firstLetter.push(w[0].toUpperCase());
+    }
+  }
+  return firstLetter.sort().join("");
 }
 
+
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
